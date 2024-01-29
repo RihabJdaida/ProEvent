3ebc6bb1345554affb3f3aafa0df5f946dc637a3
rihab.jdaida <rjdaida@asm-dev.com>
2024-01-29 15:07:17 +0100

    first commit

---
+++ src/main/java/com/example/proevent/ProEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.proevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proevent/ProEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
